e5fd5bf264757fdaa502bd3feeeb3b189bf4682f
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-05-22 08:06:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment_3._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 0, 0, 0, 0 };
+
+            Console.WriteLine("Enter first number:");
+            inputArray[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            inputArray[1] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            inputArray[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter fourth number:");
+            inputArray[3] = Convert.ToInt32(Console.ReadLine());
+
+            double[] returnArray = TotalAverage(inputArray);
+            Console.WriteLine($"The total is {returnArray[0]} and the average is {returnArray[1]}");
+            
+        }
+
+        static double[] TotalAverage(int[] methodInput)
+        {
+            int total = methodInput[0] + methodInput[1] + methodInput[2] + methodInput[3];
+            double average = total / 4;
+
+            double[] outputArray = {total, average};
+            return outputArray;
+
+            
+        }
+    }
+}
